a83fec06704246daa14c32a9cd91bbd86f2b97de
Ali Hadimoghadam <72919682+alidhdimoqdm@users.noreply.github.com>
2023-04-11 20:00:14 +0330

    Update object_detection.py

--- object_detection.py
+++ object_detection.py
@@ -1,6 +1,6 @@
 import numpy as np
 from keras.layers import Conv2D, Input, BatchNormalization, LeakyReLU, ZeroPadding2D, UpSampling2D
-from keras.layers.merge import add, concatenate
+from keras.layers import add, concatenate
 from keras.models import Model
 import struct
 import cv2
