8a376b245f5baee7ec655961c8593d1b1f1bfc25
Ali Hadimoghadam <72919682+alidhdimoqdm@users.noreply.github.com>
2023-04-12 10:58:46 +0330

    minor edits

--- object_detection.py
+++ object_detection.py
@@ -374,7 +374,7 @@ def draw_boxes(image, boxes, line, labels, obj_thresh, dcnt):
                 cimg = image[box.ymin:box.ymax, box.xmin:box.xmax]
                 cv2.imshow("violation", cimg)
                 cv2.waitKey(5)
-                cv2.imwrite("G:/Traffic Violation Detection/Traffic Signal Violation Detection System/Detected Images/violation_"+str(dcnt)+".jpg", cimg)
+                cv2.imwrite("D:/Traffic Violation Detection/Traffic Signal Violation Detection System/Detected Images/violation_"+str(dcnt)+".jpg", cimg)
                 dcnt = dcnt+1
             else:
                 cv2.rectangle(image, (box.xmin,box.ymin), (box.xmax,box.ymax), (0,255,0), 3)
@@ -388,7 +388,7 @@ def draw_boxes(image, boxes, line, labels, obj_thresh, dcnt):
         
     return image
 
-weights_path = "D:/TSVD/yolov3.weights"
+weights_path = "D:/Traffic Violation Detection/yolov3.weights"
 # set some parameters
 net_h, net_w = 416, 416
 obj_thresh, nms_thresh = 0.5, 0.45
